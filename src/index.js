852449f8672ef112a42f19fe524f548966abcf03
anattolia <anattolia@gmail.com>
2022-05-24 15:05:43 -0500

    :sparkles: Primer commit: pintar mapa

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './scss/estilos.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZW5mbHVqbyIsImEiOiJjbDNrOXNndXQwMnZsM2lvNDd4N2x0M3dvIn0.eWs4BHs67PcETEUI00T66Q';
+const mapa = new mapboxgl.Map({
+  container: 'mapa', // container ID
+  style: 'mapbox://styles/enflujo/cl3kdh8bp006b14lowcxmiwyd', // style URL
+  center: [-74.0791, 4.5462], // starting position [lng, lat]
+  zoom: 10, // starting zoom
+});
+
 console.log('..:: EnFlujo ::..');
