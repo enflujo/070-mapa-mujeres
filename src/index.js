90b5075e5d6bad1bedcf49cf52bf8173f337be6d
anattolia <anattolia@gmail.com>
2022-05-25 15:59:11 -0500

    :nail_care: Traducir comentarios

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import mapboxgl from 'mapbox-gl';
 
 const token = process.env.MAPBOX_TOKEN;
 
-if (token !== '') {
-  mapboxgl.accessToken = token;
-  const mapa = new mapboxgl.Map({
-    container: 'mapa', // container ID
-    style: 'mapbox://styles/enflujo/cl3kdh8bp006b14lowcxmiwyd', // style URL
-    center: [-74.0791, 4.5462], // starting position [lng, lat]
-    zoom: 10, // starting zoom
-  });
-}
+// Crear nuevo mapa usando un estilo y un token de MapBox
+mapboxgl.accessToken = token;
+const mapa = new mapboxgl.Map({
+  container: 'mapa', // ID del contenedor
+  style: 'mapbox://styles/enflujo/cl3kdh8bp006b14lowcxmiwyd', // URL del estilo
+  center: [-74.0791, 4.5462], // posición inicial del mapa [long, lat]
+  zoom: 10, // zoom inicial
+});
 
 console.log('..:: EnFlujo ::..');
