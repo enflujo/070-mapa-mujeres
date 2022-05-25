df4ce53c3e0b4218293fc02a864f28d97a282a75
anattolia <anattolia@gmail.com>
2022-05-25 12:30:34 -0500

    :poop: Borrar token comentado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './scss/estilos.scss';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import mapboxgl from 'mapbox-gl';
 
 const token = process.env.MAPBOX_TOKEN;
 
 if (token !== '') {
-  mapboxgl.accessToken = token; // 'pk.eyJ1IjoiZW5mbHVqbyIsImEiOiJjbDNrOXNndXQwMnZsM2lvNDd4N2x0M3dvIn0.eWs4BHs67PcETEUI00T66Q'; //process.env.MAPBOX_TOKEN;
+  mapboxgl.accessToken = token;
   const mapa = new mapboxgl.Map({
     container: 'mapa', // container ID
     style: 'mapbox://styles/enflujo/cl3kdh8bp006b14lowcxmiwyd', // style URL
