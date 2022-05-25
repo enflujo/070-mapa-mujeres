9efc4da31dd462471fc8810fd5d2a37f65605f8e
anattolia <anattolia@gmail.com>
2022-05-25 11:22:47 -0500

    :speak_no_evil: Pasar token a .env

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './scss/estilos.scss';
 import 'mapbox-gl/dist/mapbox-gl.css';
-
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZW5mbHVqbyIsImEiOiJjbDNrOXNndXQwMnZsM2lvNDd4N2x0M3dvIn0.eWs4BHs67PcETEUI00T66Q';
-const mapa = new mapboxgl.Map({
-  container: 'mapa', // container ID
-  style: 'mapbox://styles/enflujo/cl3kdh8bp006b14lowcxmiwyd', // style URL
-  center: [-74.0791, 4.5462], // starting position [lng, lat]
-  zoom: 10, // starting zoom
-});
+const token = process.env.MAPBOX_TOKEN;
+
+if (token !== '') {
+  mapboxgl.accessToken = token; // 'pk.eyJ1IjoiZW5mbHVqbyIsImEiOiJjbDNrOXNndXQwMnZsM2lvNDd4N2x0M3dvIn0.eWs4BHs67PcETEUI00T66Q'; //process.env.MAPBOX_TOKEN;
+  const mapa = new mapboxgl.Map({
+    container: 'mapa', // container ID
+    style: 'mapbox://styles/enflujo/cl3kdh8bp006b14lowcxmiwyd', // style URL
+    center: [-74.0791, 4.5462], // starting position [lng, lat]
+    zoom: 10, // starting zoom
+  });
+}
 
 console.log('..:: EnFlujo ::..');
